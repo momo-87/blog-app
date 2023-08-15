0d765c08365b4a1ce2d0e3103b0a426812caa65e
Momo-87 <momochristian87@gmail.com>
2023-08-15 14:39:14 +0100

    Create a migration to generate comments table

--- db/migrate/20230815132940_create_comments.rb
+++ db/migrate/20230815132940_create_comments.rb
@@ -1,8 +1,9 @@
-class CreateLikes < ActiveRecord::Migration[7.0]
+class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
+    create_table :comments do |t|
       t.references :UserId, null: false, foreign_key: { to_table: :users }
       t.references :PostId, null: false, foreign_key: { to_table: :posts }
+      t.text :text
       t.datetime :created_at
       t.datetime :updated_at
 
