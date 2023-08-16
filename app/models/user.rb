e3177c280d49e026c1180f7c12740af0b2c82151
Momo-87 <momochristian87@gmail.com>
2023-08-16 17:57:19 +0100

    Add has_many likes and has_many comments relationship to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   attr_accessor :posts_counter
 
   has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
