10246e974987dd6286809ed4a3ad9c21978779d7
Momo-87 <momochristian87@gmail.com>
2023-08-16 16:47:16 +0100

    Create Comment model method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   belongs_to :post
   validates_presence_of :text
+
+  def update_comments_counter_for_a_post
+    post.update(comments_counter: post.comments.count)
+  end
 end
