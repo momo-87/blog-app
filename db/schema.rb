2f10b2583b558b19296067754a4a5fdce90387ff
Momo-87 <momochristian87@gmail.com>
2023-08-16 07:39:58 +0100

    Rename user_id column to author_id in the comments and likes tables

--- db/schema.rb
+++ db/schema.rb
@@ -12,31 +12,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_815_163_236) do
+ActiveRecord::Schema[7.0].define(version: 20_230_816_061_957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
-    t.bigint 'user_id', null: false
+    t.bigint 'author_id', null: false
     t.bigint 'post_id', null: false
     t.text 'text'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_comments_on_author_id'
     t.index ['post_id'], name: 'index_comments_on_post_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
   create_table 'likes', force: :cascade do |t|
-    t.bigint 'user_id', null: false
+    t.bigint 'author_id', null: false
     t.bigint 'post_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_likes_on_author_id'
     t.index ['post_id'], name: 'index_likes_on_post_id'
-    t.index ['user_id'], name: 'index_likes_on_user_id'
   end
 end
 
-ActiveRecord::Schema.define(version: 20_230_815_163_236) do
+ActiveRecord::Schema[7.0].define(version: 20_230_816_061_957) do
   create_table 'posts', force: :cascade do |t|
     t.bigint 'author_id', null: false
     t.string 'title'
@@ -58,8 +58,8 @@ ActiveRecord::Schema.define(version: 20_230_815_163_236) do
   end
 
   add_foreign_key 'comments', 'posts'
-  add_foreign_key 'comments', 'users'
+  add_foreign_key 'comments', 'users', column: 'author_id'
   add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users'
+  add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
