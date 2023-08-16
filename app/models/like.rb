a564ab178179ac6ddbb2187a5c7492e8cc6050c8
Momo-87 <momochristian87@gmail.com>
2023-08-16 16:50:51 +0100

    Create Like model method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   belongs_to :post
+
+  def update_likes_counter_for_a_post
+    post.update(likes_counter: post.likes.counter)
+  end
 end
