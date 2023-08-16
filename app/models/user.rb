c7b7f760a1914cbfe5196dc27a2d63284780b4fa
Momo-87 <momochristian87@gmail.com>
2023-08-16 11:56:21 +0100

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+  validates :photo, presence: true,
+  validates :bio, presence: true,
+  attr_accessor :posts_counter
+
+  has_many :posts
+end
