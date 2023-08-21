8999bd1c7f1e7124f6c52d1c40f4df7c5d584310
Momo-87 <momochristian87@gmail.com>
2023-08-21 11:12:48 +0100

    Add index and show actions to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @post = @user.posts.find(params[:id])
+  end
 end
