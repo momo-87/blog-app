30cf6795e36ddb388a471529d0feb7ed5a2f5250
Momo-87 <momochristian87@gmail.com>
2023-08-21 11:46:42 +0100

    Create rountes for all URLs

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root for https://
+  root to: 'users#index'
+
+  #root for https://users/745
+  get '/users/:id', to: 'users#show' as: :user
+
+  #root for https://users/745/post
+  get '/users/author_id/posts', to: 'posts#index' as: :user_posts
+
+  #root for https://users/745/posts/3
+  get '/users/author_id/posts/:id', to: 'posts#show' as: :user_post
 end
