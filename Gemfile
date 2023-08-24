c880aaf73fa9857420901d58d0e6c86051096a72
Momo-87 <momochristian87@gmail.com>
2023-08-24 09:37:05 +0100

    Set posts-counter, comments_counter and likes_counter default value to 0

--- Gemfile
+++ Gemfile
@@ -56,7 +56,8 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'web-console'
-  gem "rails_live_reload"
+  gem 'rails_live_reload'
+  gem 'faker'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+require 'faker'
+
+Like.destroy_all
+Comment.destroy_all
+Post.destroy_all
+User.destroy_all
+
+quantity_users = 10
+quantity_posts = 10
+quantity_comments = 10
+quantity_likes = 20
+users = []
+
+for user_position in 1..quantity_users do
+  temp_user = User.create!(name: "User Number #{user_position}", photo: 'https://placehold.co/200x200', bio: Faker::Lorem.sentences(number: 12 + Random.rand(20)).join(' '), posts_counter: 0)
+  users << temp_user
+end
+
+for user_position in 0..(quantity_users - 1) do
+  for post_position in 0..Random.rand(quantity_posts) do
+    temp_post = Post.create!(author: users[user_position], title: "Post ##{post_position + 1}", text: Faker::Lorem.sentences(number: 40 + Random.rand(100)).join(' '))
+    for comment_position in 0..Random.rand(quantity_comments) do
+      position = Random.rand(quantity_users)
+      Comment.create!(post: temp_post, author: users[Random.rand(quantity_users)], text: Faker::Lorem.sentences(number: 12 + Random.rand(20)).join(' '))
+    end
+    for like in 1..Random.rand(quantity_likes) do
+      Like.create!(post: temp_post, author: users[Random.rand(quantity_users)])
+    end
+  end
+end
+
+puts "Created #{User.count} users"
+puts "Created #{Post.count} posts"
+puts "Created #{Comment.count} comments"
+puts "Assigned #{Like.count} likes, randomly"
