e70cd7e35943da0d7b377c991c3d4ef57cb75016
Momo-87 <momochristian87@gmail.com>
2023-08-31 13:33:11 +0100

    Fix secret key issue

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -103,7 +103,8 @@ Devise.setup do |config|
   # requests for sign in and sign up, you need to get a new CSRF token
   # from the server. You can disable this option at your own risk.
   # config.clean_up_csrf_token_on_authentication = true
-
+  #
+  
   # When false, Devise will not attempt to reload routes on eager load.
   # This can reduce the time taken to boot the app but if your application
   # requires the Devise mappings to be loaded during boot time the application
