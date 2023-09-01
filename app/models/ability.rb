66aaa9b19ea0ad1c43e762e79e26cc79f6319641
Fezzopro <fezzopro@gmail.com>
2023-09-01 14:21:12 +0200

    Add admin can destroy comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
     if user.role == 'admin'
       can :destroy, Post
       can :create, Like
+      can :destroy, Comment
     else
     can :destroy, Post, author_id: user.id
     can :destroy, Comment, author_id: user.id
