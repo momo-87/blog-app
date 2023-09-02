2a7de83715c37179e7ed857b4d818048d30d15aa
Fezzopro <fezzopro@gmail.com>
2023-09-02 14:39:24 +0200

    Add create action to comment API

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -7,4 +7,22 @@ class Api::CommentsController < ApplicationController
     comments = post.comments
     render json: comments
   end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.build(comment_params)
+    comment.author = current_user
+
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
